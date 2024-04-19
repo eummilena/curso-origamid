d64a0de1de19f0d60fab35632dc1150997e9243d
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-04-19 14:44:38 -0300

    up

---
+++ animais-fantasticos/script.js
@@ -0,0 +1,6 @@
+const animais = document.getElementById('animais');
+const gridSection = document.querySelectorAll('grid-section');
+const linksInternos = document.querySelector('[hrefˆ="#"]');
+const animaisImg = document.querySelectorAll('.animais img');
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
