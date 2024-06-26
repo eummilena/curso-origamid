b75b456a9533df7c625cc4f025c60dfb4ff72112
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-06-26 17:09:34 -0300

    Update modal.js

--- animais-fantasticos/js/modules/modal.js
+++ animais-fantasticos/js/modules/modal.js
@@ -1,25 +1,25 @@
-export default function initModal() {}
+export default function initModal() {
+  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+  const botaoFechar = document.querySelector('[data-modal="fechar"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
 
-const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-const botaoFechar = document.querySelector('[data-modal="fechar"]');
-const containerModal = document.querySelector('[data-modal="container"]');
+  if (botaoAbrir && botaoFechar && containerModal) {
+    function abrirModal(event) {
+      event.preventDefault();
+      container.classList.add("ativo");
+    }
 
-if (botaoAbrir && botaoFechar && containerModal) {
-  function abrirModal(event) {
-    event.preventDefault();
-    container.classList.add("ativo");
+    function fecharModal(event) {
+      event.preventDefault();
+      container.classList.remove("ativo");
+    }
+
+    function cliqueForaModal(event) {
+      console.log(event);
+      if (event.target === this) fecharModal(event);
+    }
+    botaoAbrir.addEventListener("click", abrirModal);
+    botaoFechar.addEventListener("click", fecharModal);
+    containerModal.addEventListener("click", cliqueForaModal);
   }
-
-  function fecharModal(event) {
-    event.preventDefault();
-    container.classList.remove("ativo");
-  }
-
-  function cliqueForaModal(event) {
-    console.log(event);
-    if (event.target === this) fecharModal(event);
-  }
-  botaoAbrir.addEventListener("click", abrirModal);
-  botaoFechar.addEventListener("click", fecharModal);
-  containerModal.addEventListener("click", cliqueForaModal);
 }
