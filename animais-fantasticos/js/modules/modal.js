eb4f13e920735dda1aeab121ca5eb30970a4f719
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-06-26 17:07:04 -0300

    Update modal.js

--- animais-fantasticos/js/modules/modal.js
+++ animais-fantasticos/js/modules/modal.js
@@ -4,20 +4,22 @@ const botaoAbrir = document.querySelector('[data-modal="abrir"]');
 const botaoFechar = document.querySelector('[data-modal="fechar"]');
 const containerModal = document.querySelector('[data-modal="container"]');
 
-function abrirModal(event) {
-  event.preventDefault();
-  container.classList.add("ativo");
-}
+if (botaoAbrir && botaoFechar && containerModal) {
+  function abrirModal(event) {
+    event.preventDefault();
+    container.classList.add("ativo");
+  }
 
-function fecharModal(event) {
-  event.preventDefault();
-  container.classList.remove("ativo");
-}
+  function fecharModal(event) {
+    event.preventDefault();
+    container.classList.remove("ativo");
+  }
 
-function cliqueForaModal(event) {
-  console.log(event);
-  if (event.target === this) fecharModal(event);
+  function cliqueForaModal(event) {
+    console.log(event);
+    if (event.target === this) fecharModal(event);
+  }
+  botaoAbrir.addEventListener("click", abrirModal);
+  botaoFechar.addEventListener("click", fecharModal);
+  containerModal.addEventListener("click", cliqueForaModal);
 }
-botaoAbrir.addEventListener("click", abrirModal);
-botaoFechar.addEventListener("click", fecharModal);
-containerModal.addEventListener("click", cliqueForaModal);
