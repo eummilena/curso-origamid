d85d062f72e213f0748e512d2f5fb3d91095ed18
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-06-27 15:34:53 -0300

    tooltip

---
+++ animais-fantasticos/js/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function initTooltip() {
+  const tooltip = document.querySelectorAll("[data-tooltip]");
+
+  tooltip.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver);
+  });
+
+  function onMouseOver(event) {
+    const tooltipBox = criarTooltipBox(this);
+    tooltipBox.style.top = event.pageY + "px";
+    tooltipBox.style.left = event.pageX + "px";
+
+    onMouseMove.tooltipBox = tooltipBox;
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener("mouseleave", onMouseLeave);
+    this.addEventListener("mousemove", onMouseMove);
+  }
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + "px";
+      this.tooltipBox.style.left = event.pageX + 20 + "px";
+    },
+  };
+
+  const onMouseLeave = {
+    tooltipBox: "",
+    element: "",
+    handleEvent() {
+      this.tooltipBox.remove();
+      this.element.removeEventListener("mouseleave", this);
+    },
+  };
+
+  function criarTooltipBox(element) {
+    const tooltipBox = document.createElement("div");
+    const text = element.getAttribute("aria-label");
+    tooltip.classList.add("tooltip");
+    l;
+    criarTooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+}
