d32981b1c7a824262076c0f65bc2e99961c2cddd
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-06-27 15:42:23 -0300

    Update tooltip.js

--- animais-fantasticos/js/modules/tooltip.js
+++ animais-fantasticos/js/modules/tooltip.js
@@ -1,21 +1,29 @@
 export default function initTooltip() {
-  const tooltip = document.querySelectorAll("[data-tooltip]");
+  const tooltips = document.querySelectorAll("[data-tooltip]");
 
-  tooltip.forEach((item) => {
+  tooltips.forEach((item) => {
     item.addEventListener("mouseover", onMouseOver);
   });
 
   function onMouseOver(event) {
     const tooltipBox = criarTooltipBox(this);
-    tooltipBox.style.top = event.pageY + "px";
-    tooltipBox.style.left = event.pageX + "px";
 
     onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener("mousemove", onMouseMove);
+
     onMouseLeave.tooltipBox = tooltipBox;
     onMouseLeave.element = this;
     this.addEventListener("mouseleave", onMouseLeave);
-    this.addEventListener("mousemove", onMouseMove);
   }
+
+  const onMouseLeave = {
+    handleEvent() {
+      this.tooltipBox.remove();
+      this.element.removeEventListener("mouseleave", onMouseLeave);
+      this.element.removeEventListener("mousemove", onMouseMove);
+    },
+  };
+
   const onMouseMove = {
     handleEvent(event) {
       this.tooltipBox.style.top = event.pageY + 20 + "px";
@@ -23,21 +31,11 @@ export default function initTooltip() {
     },
   };
 
-  const onMouseLeave = {
-    tooltipBox: "",
-    element: "",
-    handleEvent() {
-      this.tooltipBox.remove();
-      this.element.removeEventListener("mouseleave", this);
-    },
-  };
-
   function criarTooltipBox(element) {
     const tooltipBox = document.createElement("div");
     const text = element.getAttribute("aria-label");
-    tooltip.classList.add("tooltip");
-
-    criarTooltipBox.innerText = text;
+    tooltipBox.classList.add("tooltip");
+    tooltipBox.innerText = text;
     document.body.appendChild(tooltipBox);
     return tooltipBox;
   }
