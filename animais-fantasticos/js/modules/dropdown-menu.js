dd815cb34671329db7be6c48c2c247cee7099d87
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-06-28 15:13:55 -0300

    dropdown

---
+++ animais-fantasticos/js/modules/dropdown-menu.js
@@ -0,0 +1,19 @@
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown-menu]");
+
+  dropdownMenus.forEach((menu) => {
+    // menu.addEventListener('click', handleClick);
+    // menu.addEventListener('touchstart', handleClick);
+    //----------- outra alternativa de chamar eventos ----------
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+
+    //touchstart - é tipo o click só que a versão eé indicada para mobile
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle("active");
+  }
+}
