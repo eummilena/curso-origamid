88cbeacd1b6c54e983710c460ba970f611f9c5be
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-07-02 15:47:17 -0300

    Update dropdown-menu.js

--- animais-fantasticos/js/modules/dropdown-menu.js
+++ animais-fantasticos/js/modules/dropdown-menu.js
@@ -3,18 +3,17 @@ import outsideClick from "./outsideClick";
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll("[data-dropdown]");
 
-  dropdownMenus.forEach((menu) => {
-    // menu.addEventListener('click', handleClick);
-    // menu.addEventListener('touchstart', handleClick);
-    //----------- outra alternativa de chamar eventos ----------
-    dropdownMenus.forEach((menu) => {
-      ["touchstart", "click"].forEach((userEvent) => {
-        menu.addEventListener(userEvent, handleClick);
-      });
-    });
+  // menu.addEventListener('click', handleClick);
+  // menu.addEventListener('touchstart', handleClick);
+  //----------- outra alternativa de chamar eventos ----------
 
-    //touchstart - é tipo o click só que a versão eé indicada para mobile
+  dropdownMenus.forEach((menu) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
   });
+
+  //touchstart - é tipo o click só que a versão eé indicada para mobile
 
   function handleClick(event) {
     event.preventDefault();
